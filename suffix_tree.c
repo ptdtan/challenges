bc1c0a65195abcebf0d9f9d399ace37cd7e810bb
ptdtan <ptdtan@gmail.com>
2017-07-24 09:31:50 +0700

    updata suffix tree

--- suffix_tree.c
+++ suffix_tree.c
@@ -1,14 +1,8 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include <unistd.h>
-#include <limits.h>
-#include <assert.h>
-#include <math.h>
 #include "string.h"
-#include <ctype.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <inttypes.h>
+#include "stdint.h"
+
 #define N_CHILDS 5
 
 #define CUR(a_n, a_l, a_e) ((T*)a_n->edge[a_e])->s + a_l -1 
@@ -20,6 +14,7 @@
         a_l = 0; \
         a_e = -1;\
 
+uint32_t cnt = 0;
 unsigned char seq_nt6_table[256] = { 
 	5, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5, 
 	5, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5,  5, 5, 5, 5, 
@@ -42,15 +37,16 @@ char *nt_rev = "ACGT$";
  
 typedef struct 
 {
-    int s;
-    int *e;
+    uint32_t s;
+    uint32_t  *e;
     struct T **edge;
     struct T *p;
     struct T *f;
 } T;
 
-T * init_tree(struct T* p, int s, int *e, struct T* f)
+T * init_tree(struct T* p, uint32_t s, uint32_t *e, struct T* f)
 {
+	cnt++;
     //allocate for the new node
     T *tr = (T *)malloc(sizeof(T));
     tr->edge = (struct T **)calloc(N_CHILDS, sizeof(T*));
@@ -64,9 +60,9 @@ T * init_tree(struct T* p, int s, int *e, struct T* f)
 }
 /* create new node with left leaf node
  * */
-void new_internal(T* p, int a_e, int *ls, int n_l, int *end, int *str, T* root)
+void new_internal(T* p, int8_t a_e, uint32_t *ls, uint32_t n_l, uint32_t *end, uint8_t *str, T* root)
 {
-    int *e = malloc(4);
+    uint32_t *e = malloc(4);
     *e = *ls + 1;
     //new internal node
     T *in = init_tree(p, ((T*)p->edge[a_e])->s, e, root);
@@ -89,14 +85,14 @@ void new_internal(T* p, int a_e, int *ls, int n_l, int *end, int *str, T* root)
 
 /*new leaf node
  */
-void new_leaf(T *p, int *end, int s, int* str)
+void new_leaf(T *p, uint32_t *end, uint32_t s, uint8_t * str)
 {
     p->edge[str[s]] = init_tree(p, s, end, (T*)0);
 }
 
 /* walk down
  */
-void walk_down(int l, int i, int* str, T **a_n, int *a_l, int *a_e)
+void walk_down(uint32_t l, uint32_t i, uint8_t* str, T **a_n, uint32_t *a_l, int8_t *a_e)
 {
 	T * d = NULL;
 	while(l < i + 1){
@@ -122,21 +118,24 @@ void walk_down(int l, int i, int* str, T **a_n, int *a_l, int *a_e)
 	}
 }
 
-T * build_tree(int *str, int len)
+T * build_tree(uint8_t *str, uint32_t len)
 {
-    int i = 0, j = 1, k; 
-    int *end = malloc(4);
+    uint32_t i = 0, j = 1;
+    uint32_t *end = malloc(4);
     *end = 1;
-    T * root = init_tree(NULL, -1, (int *)NULL, NULL);
+    T * root = init_tree(NULL, 0, (uint32_t *)0, NULL);
     root->f = root;
+    root->p = root;
     root->edge[str[i]] = (struct T*)init_tree((struct T*)root, i, end, NULL);
 
     T* a_n = root;
     T *last = root;
-    int a_l = 0;
-    int a_e = -1;
-    int cur = 0;
-    int l = 0;
+    uint32_t a_l = 0;
+    int8_t a_e = -1;
+    uint32_t cur = 0;
+    uint32_t l = 0;
+    uint32_t gamma = 0;
+    uint32_t k = 0;
 
     while(i < len - 1){
         //finish rule 1 till show stopper
@@ -154,31 +153,46 @@ T * build_tree(int *str, int len)
                     break;
                 }else{
                     new_leaf(a_n, end, i + 1, str);
-                    a_n = (a_n == root)?root:a_n->f; //a_n here always non-leaf 
+                    a_n = (a_n == root)?root:a_n->f;
+                    //walk down if suffix link point to root
+                    if(a_n == root){
+                    	walk_down(j + 1, i, str, &a_n, &a_l, &a_e);
+					}
                     j++;
                     last = root;
                     continue;
                 }
             } else{
-                int k = CUR(a_n, a_l, a_e);
+                k = CUR(a_n, a_l, a_e);
                 if(str[k + 1] == str[i+1]){
                     a_l++;
                     break;
                 }else{
                     new_internal(a_n, a_e, &k, i + 1, end, str, root);
-                    //go to suffix link if exits
+                    //set suffix link from previous created internal node
                     if(last != root){
                         last->f = a_n->edge[a_e];
                     }
+                    //set new active node for suffix link
+                    //in next extension
                     last = (T*)a_n->edge[a_e];
-                    //walk up at most one edge
-                    a_n = ((T*)a_n->edge[a_e])->f == root?a_n->f:((T*)a_n->edge[a_e])->f;
-
-                    l = a_n == root ? j + 1: last->s;
+                    //go to the suffix link, walk up at most one edge
+                    if(a_n->f == root){
+                    	//increase length of have-to-walk string
+                    	gamma = a_n == root ? 0 : *a_n->e - a_n->s;
+                    	a_n = ((T*)(a_n->p))->f;
+					}else{
+						a_n = a_n->f;
+						gamma = 0;
+					}
                     //walk down
                     a_l = 0;
                     a_e = -1;
-                    walk_down(l, i, str, &a_n, &a_l, &a_e);
+                    if(a_n == root){
+                    	walk_down(j + 1, i, str, &a_n, &a_l, &a_e);
+					}else{
+						walk_down(i - (*last->e - last->s) - gamma + 1, i, str, &a_n, &a_l, &a_e);
+					}
                     j++;
                 }
             }
@@ -192,12 +206,12 @@ T * build_tree(int *str, int len)
  * print edges of suffix tree for debugging
  *
  * */
-void print_tree(T *root, int* str)
+void print_tree(T *root, uint8_t* str)
 {
 	for(int i = 0; i < N_CHILDS; ++i){
 		if(root->edge[i] != NULL){
 			T * ch = root->edge[i];
-			for(int c = ch->s; c < *ch->e ; c++)
+			for(uint32_t c = ch->s; c < *ch->e ; c++)
 				printf("%c", nt_rev[str[c]]);
 			printf("\n");
 			print_tree(ch, str);
@@ -209,9 +223,9 @@ int main(int argc, char* argv[])
 {
    FILE *fp = fopen(argv[1], "r");
     int len = 0;
-    int *str = (int *)calloc(500000000, sizeof(int));
-    int *r = str;
-    int *buff = malloc(4);
+    uint8_t *str = (uint8_t *)calloc(500000000, sizeof(uint8_t));
+    uint8_t *r = str;
+    uint8_t *buff = malloc(sizeof(uint8_t));
     while(!feof(fp)){
         fscanf(fp, "%1c", buff);
         if(buff[0] != '\n'){
@@ -222,6 +236,7 @@ int main(int argc, char* argv[])
     }
     str[len - 1] = 4;
     T * tree = build_tree(str, len);
-    print_tree(tree, str);
+    //print_tree(tree, str);
+	printf("%d\n", cnt);
     return 0;
 }
